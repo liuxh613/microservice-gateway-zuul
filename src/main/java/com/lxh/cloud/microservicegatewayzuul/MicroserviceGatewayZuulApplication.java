cb4c28b98389f8ea3b574497523f46fca250eede
liuxiaohui_yhd <liuxiaohui5@jd.com>
2018-01-31 17:37:31 +0800

    Initial commit

---
+++ src/main/java/com/lxh/cloud/microservicegatewayzuul/MicroserviceGatewayZuulApplication.java
@@ -0,0 +1,19 @@
+package com.lxh.cloud.microservicegatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class MicroserviceGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceGatewayZuulApplication.class, args);
+	}
+
+/*	@Bean
+	public PreRequestLogFilter preRequestLogFilter(){
+		return new PreRequestLogFilter();
+	}*/
+}
